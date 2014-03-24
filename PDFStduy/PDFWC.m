f39cc621036981143c725205c7082a7607c44c6d
wangdazhitech <wangdazhitech@gmail.com>
2014-03-24 20:34:26 +0800

    first commit

---
+++ PDFStduy/PDFWC.m
@@ -0,0 +1,33 @@
+//
+//  PDFWC.m
+//  PDFStduy
+//
+//  Created by wangdazhitech on 3/23/14.
+//  Copyright (c) 2014 wangdazhi. All rights reserved.
+//
+
+#import "PDFWC.h"
+
+@interface PDFWC ()
+
+@end
+
+@implementation PDFWC
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
